6abcd331d45a04e3691064d2f7818c326a2ef4bd
Stergiopoulos Valantis <c.stergiopoulos@intralot.com>
2020-03-04 13:42:04 +0200

    Add project files.

---
+++ StackOverflow/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    public class Post
+    {
+        private int _currentVote;
+        public int CurrentVote => _currentVote;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public void Upvote()
+        {
+            ++_currentVote;
+        }
+        public void Downvote()
+        {
+            --_currentVote;
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine("Date Posted: " + Timestamp.ToLongDateString());
+            Console.WriteLine(Title);
+            Console.WriteLine();
+            Console.WriteLine(Description);
+            Console.WriteLine();
+            Console.WriteLine("Votes: " + CurrentVote);
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ StackOverflow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post();
+            var post2 = new Post();
+
+            post1.Title = "Test Title";
+            post1.Description = "Blah blah blah";
+            post1.Timestamp=DateTime.Now;
+
+            post1.Upvote();
+            post1.Upvote();
+            post1.Upvote();
+            post1.Upvote();
+            post1.Downvote();
+            post1.ShowPost();
+
+            post2.Title = "Test Title for article 2";
+            post2.Description = "Blah2 blah2 blah2";
+            post2.Timestamp = DateTime.Now;
+            post2.Downvote();
+            post2.Downvote();
+            post2.Downvote();
+
+            post2.ShowPost();
+        }
+    }
+}
